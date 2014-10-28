c09e0788e12aae15370ec8199a625721ff34ba21
Kirill Sysoev <kirill.sysoev@gmail.com>
2014-10-28 09:42:19 +0300

    update

---
+++ outside/copy_to_outside.sh
@@ -0,0 +1,4 @@
+cp /opt/kazoo/applications/cccp/outside/cb_cccps.erl /opt/kazoo/applications/crossbar/src/modules/
+cp /opt/kazoo/applications/cccp/outside/cccps.json_schemas /opt/kazoo/applications/crossbar/priv/couchdb/schemas/cccps.json
+cp /opt/kazoo/applications/cccp/outside/cccps.json_accounts_views /opt/kazoo/applications/crossbar/priv/couchdb/account/cccps.json
+cp /opt/kazoo/applications/cccp/outside/cccps.json_accounts_views /opt/kazoo/applications/crossbar/priv/couchdb/views/cccps.json
