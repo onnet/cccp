a2c063bbbcbeadcef963379f5893a43296f72e9c
Kirill Sysoev <kirill.sysoev@gmail.com>
2016-07-15 10:47:18 +0000

    update

--- priv/copy_to_outside.sh
+++ priv/copy_to_outside.sh
@@ -1,4 +1,4 @@
 cp /opt/kazoo/applications/cccp/priv/cb_cccps.erl /opt/kazoo/applications/crossbar/src/modules/
 cp /opt/kazoo/applications/cccp/priv/couchdb/schemas/cccps.json /opt/kazoo/applications/crossbar/priv/couchdb/schemas/cccps.json
-cp /opt/kazoo/applications/cccp/priv/ouchdb/account/cccps.json /opt/kazoo/applications/crossbar/priv/couchdb/account/cccps.json
+cp /opt/kazoo/applications/cccp/priv/couchdb/views/cccps.json /opt/kazoo/applications/crossbar/priv/couchdb/account/cccps.json
 cp /opt/kazoo/applications/cccp/priv/couchdb/views/cccps.json /opt/kazoo/applications/crossbar/priv/couchdb/views/cccps.json
