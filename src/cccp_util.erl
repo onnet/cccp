95db84c82efd1bab6f7b7ee41b593c4a14f96897
Kirill Sysoev <kirill.sysoev@gmail.com>
2016-07-20 18:12:08 +0000

    update

--- src/cccp_util.erl
+++ src/cccp_util.erl
@@ -237,7 +237,6 @@ build_request(CallId, ToDID, AuthorizingId, Q, CtrlQ, AccountId, Action, RetainC
 -spec bridge(ne_binary(), ne_binary(), ne_binary(), ne_binary(), ne_binary(), ne_binary(), ne_binary(), ne_binary()) -> 'ok'.
 bridge(CallId, ToDID, AuthorizingId, CtrlQ, AccountId, RetainCID, RetainName, RetainNumber) ->
     Req = build_request(CallId, ToDID, AuthorizingId, 'undefined', CtrlQ, AccountId, <<"bridge">>, RetainCID, RetainName, RetainNumber),
-lager:info("IAM bridge Req: ~p",[Req]),
     kapi_resource:publish_originate_req(Req).
 
 -spec compose_cid(ne_binary(), ne_binary(), ne_binary(), ne_binary(), ne_binary()) -> {ne_binary(), ne_binary()}.
